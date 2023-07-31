7518d895473385b79e19556dd059ed509837f1d0
MianFaizanAmir0053 <97526215+MianFaizanAmir0053@users.noreply.github.com>
2023-07-31 17:28:10 +0500

    Update SongBar.jsx

--- src/components/SongBar.jsx
+++ src/components/SongBar.jsx
@@ -24,7 +24,7 @@ const SongBar = ({ song, i, artistId, isPlaying, activeSong }) => (
       />
       <div className="flex-1 flex flex-col justify-center mx-3">
         {artistId ? (
-          <Link to={`/songs/${song.id}`}>
+          <Link to={`/artists/${artistId}`}>
             <p className="text-xl font-bold text-white">
               {song?.attributes.name}
             </p>
